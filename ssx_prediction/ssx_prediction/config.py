a45cabbac2bb35ffa399ecc33535e1deb4e3b5e8
Prasanth Sivakumar <prasanth_sivakumar@hotmail.co.uk>
2020-11-23 21:11:07 +0000

    added config.py

---
+++ ssx_prediction/ssx_prediction/config.py
@@ -0,0 +1,15 @@
+# 538 XG data .csv file url 
+xg_csv = 'https://projects.fivethirtyeight.com/soccer-api/club/spi_matches.csv'
+
+# Understat site XG data url
+understat_url = "https://understat.com/league/EPL"
+
+# Supersix site fixtures data url 
+supersix_url = 'https://super6.skysports.com/play'
+
+# local chrome webdriver executable path 
+chrwebdr = 'C:\Users\Prasanth\chromedriver.exe' 
+
+# Supersix login data
+usrn = 'penstrep'
+pno = '251614'
