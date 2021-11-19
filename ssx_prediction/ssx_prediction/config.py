541ba5f525f34090ef4912bd2756eb7c25f20051
penstrep <prasanth_sivakumar@hotmail.co.uk>
2021-11-19 04:45:52 +0000

    added chrome driver settings

--- ssx_prediction/ssx_prediction/config.py
+++ ssx_prediction/ssx_prediction/config.py
@@ -1,14 +1,25 @@
-# 538 XG data .csv file url 
+from selenium import webdriver
+
+# 538 XG data .csv file url
 xg_csv = 'https://projects.fivethirtyeight.com/soccer-api/club/spi_matches.csv'
 
 # Understat site XG data url
 understat_url = "https://understat.com/league/EPL"
 
-# Supersix site fixtures data url 
+# First goal data urls
+fg_urls = {'pl': 'https://www.soccerstats.com/firstgoal.asp?league=england',
+                'ch': 'https://www.soccerstats.com/firstgoal.asp?league=england2',
+                'l1': 'https://www.soccerstats.com/firstgoal.asp?league=england3',
+                'l2': 'https://www.soccerstats.com/firstgoal.asp?league=england4'}
+
+# Supersix site fixtures data url
 supersix_url = 'https://super6.skysports.com/play'
 
-# local chrome webdriver executable path 
-cwbd_path = r'/usr/bin/chromedriver' 
+# local chrome webdriver executable path
+cwbd_path = r'/usr/bin/chromedriver'
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+driver = webdriver.Chrome(cwbd_path, options=options)
 
 # Supersix login data
 usrn = 'penstrep'
