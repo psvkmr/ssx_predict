14cb2b675b6046f977e8080740d5222ecad9c343
psvkmr <prasanth.sivakumar@genomicsengland.co.uk>
2023-07-28 12:10:48 +0100

    updated to PEP8 standards

--- ssx_prediction/xg_data.py
+++ ssx_prediction/xg_data.py
@@ -16,13 +16,11 @@ def download_xg_data(url=config.xg_csv):
     print('Finished reading in xG data file')
     return xg
 
-
-
 # xG data class --------------------------------------------------------------
 
 
-class xg_dataset:
-    
+class XgDataset:
+
     """Class of xG dataset from import of 538 statistical analysis of each match 
     across several leagues and seasons with data filtered for xG and xGDiff calculations
     
@@ -46,8 +44,10 @@ class xg_dataset:
         # download xG data as csv from URL in config.py
         self.xg = xg_data
         self.xg['date'] = pd.to_datetime(self.xg['date'], format='%Y-%m-%d')
+
+        # will create xg table
+        self.xg_table = None
                 
-        
     def get_all_leagues(self):
         
         """ Return list of all available leagues in xG dataset
@@ -55,22 +55,27 @@ class xg_dataset:
         
         return pd.Series(self.xg['league']).unique()
         
-
-    def filter_xg_dataset(self, xg_data=None, season_start_years=[2017, 2018, 2019, 2020, 2021], 
-                       list_of_leagues=['Barclays Premier League', 'English League Championship', 
-                                        'UEFA Champions League', 'UEFA Europa League', 
-                                        'English League One', 'English League Two']):
+    def filter_xg_dataset(self, xg_data=None, season_start_years=None, list_of_leagues=None):
         
         """Fitlers xG dataset provided by season year(s) and league name(s)
         
         Args:
+            xg_data: provide xg data file if not taking from method import (Optional)
             season_start_years (Optional)
             list_of_leagues (Optional)
         
         Returns:
             xg: xG dataset filtered by season year and league name
         """
-            
+
+        # set defaults
+        if season_start_years is None:
+            season_start_years = [2017, 2018, 2019, 2020, 2021]
+
+        if list_of_leagues is None:
+            list_of_leagues = ['Barclays Premier League', 'English League Championship', 'UEFA Champions League',
+                               'UEFA Europa League', 'English League One', 'English League Two']
+
         # first filter data by provided leagues
         self.xg = self.xg[self.xg['league'].isin(list_of_leagues)]
         
@@ -80,34 +85,38 @@ class xg_dataset:
         # only keep data for already completed matches up to current date
         self.xg = self.xg[self.xg['date'] < pd.Timestamp(datetime.date.today())]
 
-
-    
-    def get_team_xgs(self, season_start_years=[2020, 2021], 
-                       list_of_leagues=['Barclays Premier League']):
+    def get_team_xgs(self, season_start_years=None, list_of_leagues=None):
         
         """Applies league and season date filters for xG dataset per-match statistics
         Calculates season-average xG, xGA, and difference for each team in the dataset
         
         Args:
-            season_start (Optional)
+            season_start_years (Optional)
             list_of_leagues (Optional)
         
         Returns:
             xg_table: Dataframe with calculated xG and xGA data for each team 
             after filtering by options and collating xG info
         """
-        
+
+        # add default season start years
+        if season_start_years is None:
+            season_start_years = [2020, 2021]
+
+        if list_of_leagues is None:
+            list_of_leagues = ['Barclays Premier League']
+
         # filter by optional filters
         filt_xg = self.xg
         filt_xg = filt_xg[filt_xg['league'].isin(list_of_leagues)]
         filt_xg = filt_xg[filt_xg['season'].isin(season_start_years)]
         filt_xg = filt_xg[filt_xg['date'] < pd.Timestamp(datetime.date.today())]
         teams = pd.concat([filt_xg['team1'], filt_xg['team2']]).unique()
-        xg_stats = [pd.concat([filt_xg[filt_xg['team1']==team]['xg1'], filt_xg[filt_xg['team2']==team]['xg2']]).mean() for team in teams]
-        xga_stats = [pd.concat([filt_xg[filt_xg['team2']==team]['xg1'], filt_xg[filt_xg['team1']==team]['xg2']]).mean() for team in teams]
+        xg_stats = [pd.concat([filt_xg[filt_xg['team1'] == team]['xg1'], filt_xg[filt_xg['team2'] == team]['xg2']]).mean() for team in teams]
+        xga_stats = [pd.concat([filt_xg[filt_xg['team2'] == team]['xg1'], filt_xg[filt_xg['team1'] == team]['xg2']]).mean() for team in teams]
         
         # create dataframe of the xG and xGA data, calculate xGDiff
-        self.xg_table = pd.DataFrame({'Team':teams, 'xG':xg_stats, 'xGA':xga_stats})
+        self.xg_table = pd.DataFrame({'Team': teams, 'xG': xg_stats, 'xGA': xga_stats})
         self.xg_table['Diff'] = self.xg_table['xG'] - self.xg_table['xGA']
         self.xg_table = self.xg_table.sort_values('Diff', ascending=False)
   
@@ -115,6 +124,6 @@ class xg_dataset:
 # run if main -----------------------------------------------------------------
     
 if __name__ == '__main__':
-    xg_obj = xg_dataset( download_xg_data() )
+    xg_obj = XgDataset(download_xg_data())
     xg_obj.filter_xg_dataset()
     xg_obj.get_team_xgs()
